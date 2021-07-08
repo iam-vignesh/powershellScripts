e18599de63874b01859a9258149243b91d90c75e
Vignesh Ravi <68957716+iam-vignesh@users.noreply.github.com>
2021-07-08 09:22:26 -0400

    Update deleteTemp.ps1

--- deleteTemp.ps1
+++ deleteTemp.ps1
@@ -1,6 +1,8 @@
 <#
-The script will take an input file path and start deleting the files and folders recursively WITHOUT ASKING ANY CONFIRMATION.
-Please use it with CAUTION and this is for educational purpose only.
+The script will take an input file path and START DELETING the files and folders recursively WITHOUT ASKING ANY CONFIRMATION.
+This is for educational purpose only. The author is not responsible for any damage caused upon using the script. 
+The user assumes all responsibiliy and risk associated for the use of this script.
+Use at your own risk!
 #>
 
 $fileNames =@()
